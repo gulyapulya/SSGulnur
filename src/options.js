1c4cc756a7325639f254aacc074842a1c91ffaa5
Rohan Kaicker <rkaicker@myseneca.ca>
2022-10-02 16:42:31 -0400

    moved functionality of printing help/ version to ssg.js, started to determine config file logic

--- src/options.js
+++ src/options.js
@@ -9,6 +9,14 @@ const {version, description} = require("../package.json");
 //Get functions related to the ssg and html creation
 const ssg = require("./ssg");
 
+//NodeJS built-in core filesystem module
+//https://nodejs.org/api/fs.html
+const fs = require("fs");
+
+//NodeJS built-in core path module
+//https://nodejs.org/api/path.html
+const path = require("path");
+
 //Function for the main logic behind the command-line tool  
 //parameters: options, received from commander action handler 
 //https://www.npmjs.com/package/commander#custom-argument-processing
@@ -16,32 +24,40 @@ function ssgulnur(options) {
     //defaults
     let outputFolder = "./dist";
     let stylesheetURL = "";
+    let config = {};
+
+    if (options.config){
+        if (!fs.existsSync(options.config)){
+            console.log(chalk.bgRed("Error:") + chalk.red(" Config file does not exist."));
+            return;
+        }
+        if (path.extname(options.config) != ".json"){
+            console.log(chalk.bgRed("Error:") + chalk.red(" Config file must be a .json file."));
+            return;
+        }
+        if (Object.keys(config).length == 0){
+            console.log(chalk.bgRed("Error:") + chalk.red(" Config file is empty"));
+            return;
+        }
+        config = require(options.config);
+    }
 
     if (options.version) {
-        console.log(chalk.green.bold("SSGulnur: ") + chalk.green(version));
-        return;
+        return ssg.printVersion(version);
     }
     if (options.help) {
-        console.log(chalk.green.bold("Help guide"));
-        console.log(chalk.gray(description));
-        console.log(chalk.green("Usage:"));
-        console.log(chalk.green.dim("ssgulnur -v | --version") + chalk.gray(" current version"));
-        console.log(chalk.green.dim("ssgulnur -h | --help") + chalk.gray(" help guide"));
-        console.log(chalk.green.dim("ssgulnur -i | --input <source>") + chalk.gray(" specify a file or folder to use"));
-        console.log(chalk.green("Options for input:"));
-        console.log(chalk.green.dim("-o | --output <folder>") + chalk.gray(" specify an output folder for produced html"));
-        console.log(chalk.gray("By default, the output folder would be ./dist unless specified otherwise"));
-        console.log(chalk.green.dim("-s | --stylesheet <url>") + chalk.gray(" specify a stylesheet url to use"));
-        console.log(chalk.gray("For example, https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"));
-        return;
+        return ssg.printHelp(description);
     } 
-    if (options.input) {
-        if (options.output) {
-            outputFolder = options.output;
-        } if (options.stylesheet) {
-            stylesheetURL = options.stylesheet;
+
+    if (options.input || config.input) {
+        let input = options.input || config.input;
+        if (options.output || config.output) {
+            outputFolder = options.output || config.output;
+        } 
+        if (options.stylesheet || config.stylesheet) {
+            stylesheetURL = options.stylesheet || config.stylesheet;
         }
-        ssg(options.input, outputFolder, stylesheetURL);
+        ssg.input(input, outputFolder, stylesheetURL);
     } else {
         console.log(chalk.bgRed("Error:") + chalk.red(" unknown command or option, please see ssgulnur -help for available options."));
     }
