58810ccb647afc124662a93ddedf9313ec223ebb
Maxim N <mnosov622@gmail.com>
2022-11-14 08:03:02 +0700

    added tests for ssgulnur, createFolder and parseJSON functions

--- test/unit/helper.test.js
+++ test/unit/helper.test.js
@@ -1,4 +1,4 @@
-const { checkSource } = require('../../src/helper');
+const { checkSource, createFolder, parseJSON } = require('../../src/helper');
 
 describe('checkSource', () => {
   test('invalid path should return "none"', () => {
@@ -11,5 +11,15 @@ describe('checkSource', () => {
 
   test('path to valid file should return "file"', () => {
     expect(checkSource('test/unit/helper.test.js')).toBe('file');
+  });
+});
+
+describe('testing functions for output', () => {
+  test('nothing passed to function should return false', () => {
+    expect(createFolder()).toBeFalsy();
+  });
+
+  test('wrong path passed to function should return null', () => {
+    expect(parseJSON('wrong path')).toBe(null);
   });
 });
